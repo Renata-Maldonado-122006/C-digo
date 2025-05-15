4ffe02465449b4a6fdc2c02064c6694457773a2c
Renata-Maldonado-122006 <rubiabigail12pv@gmail.com>
2025-05-14 23:59:25 -0600

    Add files via upload

---
+++ tarea 2.0.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int *p, y;
+    void func () {
+      
+      int x=40;
+      p= &x;
+      y= *p;
+      *p= 23;
+    }
+
+int main(void) {
+    clock_t inicio, fin;
+    double tiempo;
+    inicio = clock ();
+    for (int i = 0; i < 1000000; i++) {
+    }
+    fin = clock ();
+    printf("Tiempo de ejecución: %f \n", tiempo);
+   
+   func () ;
+   y = *p;
+   *p = 25;
+   
+   printf("El valor de y es %d \n El valor de *p es %d \n El valor de p es %p", y, *p, p);
+   
+   return 0;
+ } 
+
